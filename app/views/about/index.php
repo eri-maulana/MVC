16b92303bc65c43e1719999fc890600af8096a34
eri-maulana <maulanabouble11@gmail.com>
2024-01-17 14:29:38 +0700

    5. Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,8 @@
-<h1>About Me</h1>
-<p>Hallo nama saya <?= $data['nama'] ?> saya adalah seorang <?= $data['pekerjaan'] ?>, usia saya sekarang adalah
-   <?= $data['umur'] ?></p>
+<div class="container mt-3">
+
+   <h1>About Me</h1>
+   <img src="<?= BASEURL; ?>/img/kecil.png" alt="Eri Maulana" width="200" height="250" class="rounded-circle img-thumbnail shadow my-3">
+   <p>Hallo nama saya <?= $data['nama'] ?> saya adalah seorang <?= $data['pekerjaan'] ?>, usia saya sekarang adalah
+      <?= $data['umur'] ?></p>
+
+</div>
