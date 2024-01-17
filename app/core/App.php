b772830c3e1c7b5689887e4df2766f704bbd5bae
eri-maulana <maulanabouble11@gmail.com>
2024-01-17 08:59:06 +0700

    2. Routing

--- app/core/App.php
+++ app/core/App.php
@@ -2,7 +2,18 @@
    class App{
       public function __construct()
       {
-         echo 'Ok!';
+      $url = $this->parseUrl();
+      var_dump($url);
+   }
+
+   public function parseUrl()
+   {
+      if (isset($_GET['url'])) {
+         $url = rtrim($_GET['url'], '/');
+         $url = filter_var($url, FILTER_SANITIZE_URL);
+         $url = explode('/', $url);
+         return $url;
       }
+   }
    }
  ?>
