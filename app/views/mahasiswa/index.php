64f31c37b2d673948f1e638f6238898a207f7da6
eri-maulana <maulanabouble11@gmail.com>
2024-01-18 11:37:09 +0700

    8. Insert Data #noted 'pelajari lebih dalam tentang mysql'

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -2,6 +2,11 @@
 
    <div class="row">
       <div class="col-6">
+         <!-- Button trigger modal -->
+         <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
+            Tambah Data Mahasiswa
+         </button>
+
          <h3>Daftar Mahasiswa</h3>
          <ul class="list-group">
             <?php
@@ -17,4 +22,55 @@
       </div>
    </div>
 
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="formModalLabel" aria-hidden="true">
+   <div class="modal-dialog" role="document">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+         <div class="modal-body">
+
+            <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+               <input type="hidden" name="id" id="id">
+               <div class="form-group">
+                  <label for="nama">Nama</label>
+                  <input type="text" class="form-control" id="nama" name="nama" autocomplete="off" required>
+               </div>
+
+               <div class="form-group">
+                  <label for="nrp">NRP</label>
+                  <input type="number" class="form-control" id="nrp" name="nrp" autocomplete="off">
+               </div>
+
+               <div class="form-group">
+                  <label for="email">Email</label>
+                  <input type="email" class="form-control" id="email" name="email" placeholder="email@example.com">
+               </div>
+
+               <div class="form-group">
+                  <label for="jurusan">Jurusan</label>
+                  <select class="form-control" id="jurusan" name="jurusan">
+                     <option value="Teknik Informatika">Teknik Informatika</option>
+                     <option value="Teknik Mesin">Teknik Mesin</option>
+                     <option value="Teknik Industri">Teknik Industri</option>
+                     <option value="Teknik Pangan">Teknik Pangan</option>
+                     <option value="Teknik Planologi">Teknik Planologi</option>
+                     <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+                  </select>
+               </div>
+
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Tambah Data</button>
+            </form>
+         </div>
+      </div>
+   </div>
 </div>
